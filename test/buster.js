52ccb02580a9f26ab7f40eb40517a60da672bb49
Rod Vagg <rod@vagg.org>
2012-01-15 15:12:14 +1100

    move vendor files to tests directory

--- test/buster.js
+++ test/buster.js
@@ -3,12 +3,12 @@ var config = module.exports
 config['Traversty Tests'] = {
     environment: 'browser'
   , sources: [
-        '../vendor/qwery.js'
-      , '../vendor/qwery_pseudos.js'
-      , '../vendor/sizzle.js'
-      , '../vendor/nwmatcher.js'
-      , '../vendor/es5-basic_mod.js'
-      , '../vendor/sel.js'
+        'vendor/qwery.js'
+      , 'vendor/qwery_pseudos.js'
+      , 'vendor/sizzle.js'
+      , 'vendor/nwmatcher.js'
+      , 'vendor/es5-basic_mod.js'
+      , 'vendor/sel.js'
       , 'noconflict.js'
       , '../src/traversty.js'
       , 'setup.js'
