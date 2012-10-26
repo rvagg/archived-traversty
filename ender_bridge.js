b1c8fea59d4ff3a9213fbd22b747ce206728a74a
Rod Vagg <rod@vagg.org>
2012-10-26 23:12:11 +1100

    update bridge for new methods

--- ender_bridge.js
+++ ender_bridge.js
@@ -3,37 +3,24 @@
 (function ($) {
   var t = require('traversty')
     , integrated = false
-    , integrate = function(meth) {
+    , integrate = function (meth) {
         // this crazyness is for lazy initialisation because we can't be guaranteed
         // that a selector engine has been installed *before* traversty in an ender build
-        var fn = function(self, selector, index) {
+        var fn = function (self, selector, index) {
             if (!integrated) {
               try {
                 t.setSelectorEngine($)
               } catch (ex) { } // ignore exception, we may have an ender build with no selector engine
               integrated = true
             }
-            fn = function(self, selector, index) { return $(t(self)[meth](selector, index)) }
+            fn = function (self, selector, index) { return $(t(self)[meth](selector, index)) }
             return fn(self, selector, index)
           }
-        return function(selector, index) { return fn(this, selector, index) }
+        return function (selector, index) { return fn(this, selector, index) }
       }
-    , up = integrate('up')
-    , down = integrate('down')
-    , next = integrate('next')
-    , previous = integrate('previous')
+    , methods = 'up down next previous parents siblings children first last eq slice filter not is has'.split(' ')
+    , b = {}, i = methods.length
 
-  $.ender(
-      {
-          // core
-          up: up
-        , down: down
-        , next: next
-        , previous: previous
-          // aliases
-        , parent: up
-        , prev: previous
-      }
-    , true
-  )
+  while (--i >= 0) b[methods[i]] = integrate(methods[i])
+  $.ender(b, true)
 }(ender))
