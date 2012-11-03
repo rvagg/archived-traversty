4033016e95236f5a48cfcbbb148f168b5089287e
Rod Vagg <rod@vagg.org>
2012-11-03 12:05:15 +1100

    new build system & sizes on readme

---
+++ build.js
@@ -0,0 +1,56 @@
+var fs = require('fs')
+  , gz = require('zlib').gzip
+  , colorsTmpl = require('colors-tmpl')
+  , uglify = require('uglify-js')
+
+  , inFile = 'traversty.js'
+  , outFile = 'traversty.min.js'
+  , readme = 'README.md'
+
+  , readmeContents = fs.readFileSync(readme).toString()
+  , rawContents = fs.readFileSync(inFile)
+  , copyright = rawContents.toString().match(/\/\*[\s\S]+?\*\//i)[0]
+  , minContentsPrev = fs.readFileSync(outFile)
+  , minContents = copyright + '\n' + uglify(rawContents.toString())
+
+  , sizes = {
+        raw: rawContents.length
+      , minPrev: minContentsPrev.length
+      , min: minContents.length
+    }
+
+  , s2s = function (size) {
+      size = Math.round(size / 1024 * 10) / 10;
+      return size + ' kB'
+    }
+
+sizes.min = minContents.length
+
+gz(minContentsPrev, function (err, gzContentsPrev) {
+  if (err) throw err;
+  gz(minContents, function (err, gzContents) {
+    if (err) throw err;
+
+    sizes.gz = gzContents.length
+    sizes.gzPrev = gzContentsPrev.length
+
+    for (var p in sizes) {
+      readmeContents = readmeContents.replace(
+        new RegExp(
+            '(?:(?:[\\d\\.]*|NaN)\\s*kB)\\s*(' + p + ')'
+          , 'g')
+        , s2s(sizes[p]) + ' $1');
+    }
+
+    fs.writeFileSync(outFile, minContents)
+    fs.writeFileSync(readme, readmeContents)
+
+    console.log(colorsTmpl(
+        '{bold}{green}Build successful!{/green}{/bold}\n'
+      + '\t{yellow}Previous min size:{/yellow} ' + sizes.minPrev + ' bytes\n'
+      + '\t{green}New min size     :{/green} ' + sizes.min + ' bytes\n'
+      + '\t{yellow}Previous gz size :{/yellow} ' + sizes.gzPrev + ' bytes\n'
+      + '\t{green}New gz size      :{/green} ' + sizes.gz + ' bytes'
+    ))
+  })
+})
